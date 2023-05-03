188f05dbbfae8e58c0732056ca25f6cbe86cfff3
NeelimaJuthada <132416012+NeelimaJuthada@users.noreply.github.com>
2023-05-03 21:31:27 +0530

    Add files via upload

---
+++ wrapperclass.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class Main{
+    public static void main(String args[]){
+    
+    Boolean a = true;
+    Character b = '@';
+    Integer c = 123;
+    Double d = 3.14;
+    String s = "neelima";
+    
+    if(d ==3.14){
+        System.out.println("It is TRUE ");
+    }
+  }
+    
+}
