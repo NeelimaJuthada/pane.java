53c45b5ffc3e871b42c9473110ac0e1af317fbec
NeelimaJuthada <132416012+NeelimaJuthada@users.noreply.github.com>
2023-05-03 21:48:01 +0530

    Create ArrayList-1.java

---
+++ ArrayList-1.java
@@ -0,0 +1,17 @@
+/*NOTE: ArrayList ---> package*/
+
+import java.util.ArrayList;
+public class Main{
+    public static void main(String args[]){
+     
+     ArrayList<String> food = new ArrayList<String>();
+     
+     food.add("pizza");
+     food.add("handburgger");
+     food.add("hotdogs");
+     
+     for(int i=0; i<food.size();i++){
+         System.out.println(food.get(i));
+     }
+     } 
+}
